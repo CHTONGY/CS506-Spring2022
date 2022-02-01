c3c5e5a823159a12b0cf57862aa81fd14abe5d2f
CHTONGY <ty760206236@gmail.com>
2022-02-01 09:56:02 -0500

    print library

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,12 @@
+import tools
 def draw_library():
-    print("library not found")
+    # print("library not found")
+    name = 'library'
+    name_len = len(name)
+    # print('-----------')
+    tools.print_row(name_len)
+    # print('| library |')
+    tools.print_name(name)
+    # print('-----------')
+    tools.print_row(name_len)
     return
