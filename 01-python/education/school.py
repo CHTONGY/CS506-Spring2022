e87e8991410c48d7068aff15649209a79ac2aa79
CHTONGY <ty760206236@gmail.com>
2022-02-01 09:55:55 -0500

    print school

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,12 @@
+import tools
 def draw_school():
-    print("school not found")
+    name = 'school'
+    name_len = len(name)
+    # print("school not found")
+    # print('-----------')
+    tools.print_row(name_len)
+    # print('| school |')
+    tools.print_name(name)
+    # print('-----------')
+    tools.print_row(name_len)
     return
